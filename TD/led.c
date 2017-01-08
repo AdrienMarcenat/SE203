ec40f9736069a46079a366ebe2a6d81c4d6d9ad8
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-08 23:36:15 +0100

    set pins PTD5 and PTE29 as outputs

--- TD/led.c
+++ TD/led.c
@@ -7,8 +7,9 @@ void led_init()
 
     uint32_t GPIO = 1 << 8 | ~(1 << 9 | 1 << 10); // 001 in bits [10:8]
 
-    PORTD_PCR5 &= GPIO; // set PORTD as a GPIO
-          
-    PORTE_PCR29 &= GPIO; // set PORTE as a GPIO
+    PORTD_PCR5  &= GPIO; // set pin PTD5 as a GPIO      
+    PORTE_PCR29 &= GPIO; // set pin PTE29 as a GPIO
 
+    GPIOD_PDDR |= 1 << 5 ; // set pin PTD5 as output
+    GPIOE_PDDR |= 1 << 29; // set pin PTE29 as output
 }
--- TD/led.h
+++ TD/led.h
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
-#define CLOCK_REG (*(volatile uint32_t *) 0x40048038)
-#define PORTD_PCR5 (*(volatile uint32_t *) 0x4004c014)
+#define CLOCK_REG   (*(volatile uint32_t *) 0x40048038)
+#define PORTD_PCR5  (*(volatile uint32_t *) 0x4004c014)
 #define PORTE_PCR29 (*(volatile uint32_t *) 0x4004d074)
+#define GPIOD_PDDR  (*(volatile uint32_t *) 0x400ff0d4)
+#define GPIOE_PDDR  (*(volatile uint32_t *) 0x400ff114)
