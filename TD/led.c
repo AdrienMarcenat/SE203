88d78e0b5a861b29d4808b77ece4b9afdbbb83d4
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-08 23:27:45 +0100

    set PORTE and PORTD as GPIO

--- TD/led.c
+++ TD/led.c
@@ -2,6 +2,13 @@
 
 void led_init()
 {
-    CLOCK_REG = CLOCK_REG | 1 << 12 
-                          | 1 << 13; 
+    CLOCK_REG  |= 1 << 12 
+               |  1 << 13;  // Activates the clock for PORTD and PORTE
+
+    uint32_t GPIO = 1 << 8 | ~(1 << 9 | 1 << 10); // 001 in bits [10:8]
+
+    PORTD_PCR5 &= GPIO; // set PORTD as a GPIO
+          
+    PORTE_PCR29 &= GPIO; // set PORTE as a GPIO
+
 }
--- TD/led.h
+++ TD/led.h
@@ -1,3 +1,5 @@
 #include <stdint.h>
 
 #define CLOCK_REG (*(volatile uint32_t *) 0x40048038)
+#define PORTD_PCR5 (*(volatile uint32_t *) 0x4004c014)
+#define PORTE_PCR29 (*(volatile uint32_t *) 0x4004d074)
