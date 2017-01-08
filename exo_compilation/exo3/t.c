c7c78899ba37f17183df100c8b202a793ca18d0c
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-08 21:07:28 +0100

    init exo3

---
+++ exo_compilation/exo3/t.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+
+const char mesg[] = "Hello World!\n";
+const char mesg2[] = "Hello World2!\n";
+
+int main () 
+{
+    printf(mesg);
+     
+    return 0;
+}
