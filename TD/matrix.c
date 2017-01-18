b4050b7b43076c8c0d66fcf7a83f36a977328f7a
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-18 16:50:26 +0100

    add static to the private function of matrix and move the macros to the .c

--- TD/matrix.c
+++ TD/matrix.c
@@ -2,7 +2,126 @@
 #include "bits.h"
 #include "matrix.h"
 
+#define RST(x) do{ if(x) setBit(2, &GPIOB_PSOR); \
+                   else  setBit(2, &GPIOB_PCOR); }while(0)
+
+#define SB(x)  do{ if(x) setBit(0, &GPIOB_PSOR); \
+                   else  setBit(0, &GPIOB_PCOR); }while(0) 
+
+#define LAT(x) do{ if(x) setBit(1, &GPIOB_PSOR); \
+                   else  setBit(1, &GPIOB_PCOR); }while(0) 
+
+#define SCK(x) do{ if(x) setBit(8, &GPIOC_PSOR); \
+                   else  setBit(8, &GPIOC_PCOR); }while(0) 
+
+#define SDA(x) do{ if(x) setBit(9, &GPIOC_PSOR); \
+                   else  setBit(9, &GPIOC_PCOR); }while(0) 
+
+#define ROW0(x) do{ if(x) setBit(13, &GPIOA_PSOR); \
+                    else  setBit(13, &GPIOA_PCOR);}while(0) 
+ 
+#define ROW1(x) do{ if(x) setBit(2 , &GPIOD_PSOR); \
+                    else  setBit(2 , &GPIOD_PCOR); }while(0) 
+
+#define ROW2(x) do{ if(x) setBit(4 , &GPIOD_PSOR); \
+                    else  setBit(4 , &GPIOD_PCOR); }while(0) 
+
+#define ROW3(x) do{ if(x) setBit(6 , &GPIOD_PSOR); \
+                    else  setBit(6 , &GPIOD_PCOR); }while(0) 
+
+#define ROW4(x) do{ if(x) setBit(7 , &GPIOD_PSOR); \
+                    else  setBit(7 , &GPIOD_PCOR); }while(0) 
+
+#define ROW5(x) do{ if(x) setBit(5 , &GPIOD_PSOR); \
+                    else  setBit(5 , &GPIOD_PCOR); }while(0) 
+
+#define ROW6(x) do{ if(x) setBit(12, &GPIOA_PSOR); \
+                    else  setBit(12, &GPIOA_PCOR); }while(0) 
+
+#define ROW7(x) do{ if(x) setBit(4 , &GPIOA_PSOR); \
+                    else  setBit(4 , &GPIOA_PCOR); }while(0) 
+
 extern uint8_t _binary_image_raw_start;
+
+static void pulse_SCK()
+{
+    SCK(0);
+    asm volatile ("nop");
+    SCK(1);
+    asm volatile ("nop");
+    SCK(0);
+}
+
+static void pulse_LAT()
+{
+    LAT(1);
+    asm volatile ("nop");
+    LAT(0);
+    asm volatile ("nop");
+    LAT(1);
+}
+
+static void deactivate_rows()
+{
+    ROW0(0);
+    ROW1(0);
+    ROW2(0);
+    ROW3(0);
+    ROW4(0);
+    ROW5(0);
+    ROW6(0);
+    ROW7(0);
+}
+
+static void activate_row(int row)
+{
+    switch(row)
+    {
+        case 0: { ROW0(1); break; } 
+        case 1: { ROW1(1); break; } 
+        case 2: { ROW2(1); break; } 
+        case 3: { ROW3(1); break; } 
+        case 4: { ROW4(1); break; } 
+        case 5: { ROW5(1); break; } 
+        case 6: { ROW6(1); break; } 
+        case 7: { ROW7(1); break; } 
+    }
+}
+
+static void send_bytes(uint8_t val, int bank)
+{
+    SB(bank); 
+    int i = bank ? 7 : 5; // bank1 stored 8-bit values, bank0 stored 6bit values
+    for(; i >= 0; i--)    // most significant bit first
+    {
+        SDA(getBit8(val, i));
+        pulse_SCK();
+    }
+}
+
+static void mat_set_row(int row, const rgb_color *val)
+{
+    for(int i = 7; i >= 0; i--) // left LEDs first
+    {
+        send_bytes(val[i].b, 1);
+        send_bytes(val[i].g, 1);
+        send_bytes(val[i].r, 1);
+    }
+    deactivate_rows();
+    for(int i = 0; i < 50; i++)  
+        asm volatile ("nop"); 
+    pulse_LAT();
+    activate_row(row);
+}
+
+static void init_bank0()
+{
+    for(int i = 0; i < 18; i++)
+    {
+        send_bytes(0xff, 0);
+    }
+    pulse_LAT();
+}
 
 void matrix_init()
 {
@@ -54,84 +173,14 @@ void matrix_init()
     init_bank0();
 }
 
-void pulse_SCK()
+static void fill_line(rgb_color* line, uint8_t* add)
 {
-    SCK(0);
-    asm volatile ("nop");
-    SCK(1);
-    asm volatile ("nop");
-    SCK(0);
-}
-
-void pulse_LAT()
-{
-    LAT(1);
-    asm volatile ("nop");
-    LAT(0);
-    asm volatile ("nop");
-    LAT(1);
-}
-
-void deactivate_rows()
-{
-    ROW0(0);
-    ROW1(0);
-    ROW2(0);
-    ROW3(0);
-    ROW4(0);
-    ROW5(0);
-    ROW6(0);
-    ROW7(0);
-}
-
-void activate_row(int row)
-{
-    switch(row)
+    for(int j = 0; j < 8; j++)
     {
-        case 0: { ROW0(1); break; } 
-        case 1: { ROW1(1); break; } 
-        case 2: { ROW2(1); break; } 
-        case 3: { ROW3(1); break; } 
-        case 4: { ROW4(1); break; } 
-        case 5: { ROW5(1); break; } 
-        case 6: { ROW6(1); break; } 
-        case 7: { ROW7(1); break; } 
+        rgb_color color = {*add, *(add+1), *(add+2)};
+        line[j] = color; 
+        add += 3;
     }
-}
-
-void send_bytes(uint8_t val, int bank)
-{
-    SB(bank); 
-    int i = bank ? 7 : 5; // bank1 stored 8-bit values, bank0 stored 6bit values
-    for(; i >= 0; i--)    // most significant bit first
-    {
-        SDA(getBit8(val, i));
-        pulse_SCK();
-    }
-}
-
-void mat_set_row(int row, const rgb_color *val)
-{
-    for(int i = 7; i >= 0; i--) // left LEDs first
-    {
-        send_bytes(val[i].b, 1);
-        send_bytes(val[i].g, 1);
-        send_bytes(val[i].r, 1);
-    }
-    deactivate_rows();
-    for(int i = 0; i < 50; i++)  
-        asm volatile ("nop"); 
-    pulse_LAT();
-    activate_row(row);
-}
-
-void init_bank0()
-{
-    for(int i = 0; i < 18; i++)
-    {
-        send_bytes(0xff, 0);
-    }
-    pulse_LAT();
 }
 
 void test_pixels()
@@ -187,15 +236,5 @@ void test_image()
         mat_set_row(5, lines[5]);
         mat_set_row(6, lines[6]);
         mat_set_row(7, lines[7]);
-    }
-}
-
-void fill_line(rgb_color* line, uint8_t* add)
-{
-    for(int j = 0; j < 8; j++)
-    {
-        rgb_color color = {*add, *(add+1), *(add+2)};
-        line[j] = color; 
-        add += 3;
     }
 }
--- TD/matrix.h
+++ TD/matrix.h
@@ -11,55 +11,7 @@ typedef struct
 } rgb_color;
 
 void matrix_init();
-void deactivate_rows();
-void activate_row(int row);
-void pulse_SCK();
-void pulse_LAT();
-void send_bytes(uint8_t, int);
-void mat_set_row(int, const rgb_color*);
-void init_bank0();
 void test_pixels();
 void test_image();
-void fill_line(rgb_color*, uint8_t*);
-
-#define RST(x) do{ if(x) setBit(2, &GPIOB_PSOR); \
-                   else  setBit(2, &GPIOB_PCOR); }while(0)
-
-#define SB(x)  do{ if(x) setBit(0, &GPIOB_PSOR); \
-                   else  setBit(0, &GPIOB_PCOR); }while(0) 
-
-#define LAT(x) do{ if(x) setBit(1, &GPIOB_PSOR); \
-                   else  setBit(1, &GPIOB_PCOR); }while(0) 
-
-#define SCK(x) do{ if(x) setBit(8, &GPIOC_PSOR); \
-                   else  setBit(8, &GPIOC_PCOR); }while(0) 
-
-#define SDA(x) do{ if(x) setBit(9, &GPIOC_PSOR); \
-                   else  setBit(9, &GPIOC_PCOR); }while(0) 
-
-#define ROW0(x) do{ if(x) setBit(13, &GPIOA_PSOR); \
-                    else  setBit(13, &GPIOA_PCOR);}while(0) 
- 
-#define ROW1(x) do{ if(x) setBit(2 , &GPIOD_PSOR); \
-                    else  setBit(2 , &GPIOD_PCOR); }while(0) 
-
-#define ROW2(x) do{ if(x) setBit(4 , &GPIOD_PSOR); \
-                    else  setBit(4 , &GPIOD_PCOR); }while(0) 
-
-#define ROW3(x) do{ if(x) setBit(6 , &GPIOD_PSOR); \
-                    else  setBit(6 , &GPIOD_PCOR); }while(0) 
-
-#define ROW4(x) do{ if(x) setBit(7 , &GPIOD_PSOR); \
-                    else  setBit(7 , &GPIOD_PCOR); }while(0) 
-
-#define ROW5(x) do{ if(x) setBit(5 , &GPIOD_PSOR); \
-                    else  setBit(5 , &GPIOD_PCOR); }while(0) 
-
-#define ROW6(x) do{ if(x) setBit(12, &GPIOA_PSOR); \
-                    else  setBit(12, &GPIOA_PCOR); }while(0) 
-
-#define ROW7(x) do{ if(x) setBit(4 , &GPIOA_PSOR); \
-                    else  setBit(4 , &GPIOA_PCOR); }while(0) 
-
 
 #endif
