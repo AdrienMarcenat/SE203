c0300e87f826d1a39370056aeb9fcff6c13351c0
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-20 13:48:13 +0100

    code review: initialisation of all the UART registers

--- TD/src/uart.c
+++ TD/src/uart.c
@@ -3,47 +3,46 @@
 #include "reg.h"
 #include "led.h"
 
-int UART_SUM = 0; // Global variable used to store the checksum(see uart_checksum())
-
 void uart_init()
 {
     clearBit(27, &SIM_SOPT2);
     setBit(26, &SIM_SOPT2); // use MCGPLLCLK clock for UART0
     setBit(16, &SIM_SOPT2); // divide MGPLLCLK by 2
-    setBit(10, &SIM_SCGC4); // clock enabled for UART0
+    UART0_ENABLED();
 
-    UARTO_C2 = 0;                          // UART0 deactivated
+    // Initialisation of all the UART registers
+    UART0_C2 = 0;                          
+    UART0_S1 = 0;
+    UART0_S2 = 0;
+    UART0_C1 = 0;
+    set_and_clear8(&UART0_C3, 0x00, 0b00001111);
+    set_and_clear8(&UART0_C5, 0b01011100, 0x00);
+    set_and_clear8(&UART0_C4, 0x00, 0b00011101); // 11101 = 29 (115200)
+    UART0_MA1 = 0;
+    UART0_MA2 = 0;
+    UART0_BDH = 0;
+    set_and_clear8(&UART0_BDL, 0x00, 0x7); // set bits [7:0] to 0b111 (0d7)
 
-    set_and_clear8(&UARTO_C4, 0xe0, 0x1d); // set bits [4:0] to 0b11101 (0d29)
-
-    clearBit8(5, &UARTO_BDH);              // one stop bit
-    set_and_clear8(&UARTO_BDL, 0x00, 0x7); // set bits [7:0] to 0b111 (0d7)
-    set_and_clear8(&UARTO_BDH, 0xf0, 0x0); // set bits [3:0] to 0
-
-    clearBit8(1, &UARTO_C1);               // parity disabled
-    clearBit8(4, &UARTO_C1);               // 8-bit mode
-
-    setBit(9, &SIM_SCGC5);                 // activate PORTA clock
-
+    PORTA_ENABLED();
     set_and_clear(&PORTA_PCR1, 0xfffff8ff, 0x200); // alt2 = 010 (RX)
     set_and_clear(&PORTA_PCR2, 0xfffff8ff, 0x200); // alt2 = 010 (TX)
 
-    setBit8(2, &UARTO_C2); // Receiver enabled
-    setBit8(3, &UARTO_C2); // Transmitter enabled
+    setBit8(2, &UART0_C2); // Receiver enabled
+    setBit8(3, &UART0_C2); // Transmitter enabled
 }
 
 void uart_putchar(char c)
 {
-    while(getBit8(UARTO_S1,7) == 0); // While transmitter is active
+    while(getBit8(UART0_S1,7) == 0); // While transmitter is active
 
-    UARTO_D = c;
+    UART0_D = c;
 }
 
 unsigned char uart_getchar()
 {
-    while(getBit8(UARTO_S1,5) == 0); // While receive data buffer is empty
+    while(getBit8(UART0_S1,5) == 0); // While receive data buffer is empty
 
-    return UARTO_D;
+    return UART0_D;
 }
 
 void uart_puts(const char *s)
@@ -68,17 +67,4 @@ void uart_gets(char *s, int size)
         }
     }
     s[i] = 0;
-}
-
-void uart_checksum()
-{
-    for(int i = 0 ; i<1000 ;i++) // We expect that the checksum.py program sent 1000 char
-    {
-        UART_SUM += (int)uart_getchar();
-    }
-}
-
-void uart_echo()
-{
-    uart_putchar(uart_getchar());
 }
--- TD/src/uart.h
+++ TD/src/uart.h
@@ -1,12 +1,8 @@
 #ifndef UART_H
 #define UART_H
 
-int UART_SUM; // Global variable used by uart_checksum()
-
 void uart_init();
 void uart_putchar(char);
 unsigned char uart_getchar();
-void uart_checksum();
-void uart_echo();
 
 #endif
