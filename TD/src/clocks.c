0b89d8d75799f2f94cdd290b59455b0db56e0f5e
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-30 09:19:08 +0100

    clocks_init() works (red led is blinking)

--- TD/src/clocks.c
+++ TD/src/clocks.c
@@ -5,4 +5,16 @@
 void clocks_init()
 {
     SIM_COPC = 0; // disable watchdog
+
+    MCG_C2 = 0x2C;
+    MCG_C1 = 0xa8;
+    while(getBit8(MCG_S, 1) == 0);
+    while(getBit8(MCG_S, 4) > 0);
+    while(((MCG_S >> 2) & 0x3) != 0x2);
+    MCG_C5 = 0x03;
+    MCG_C6 = 0x40;
+    while(getBit8(MCG_S, 5) == 0);
+    while(getBit8(MCG_S, 6) == 0);
+    MCG_C1 = 0x28;
+    while(((MCG_S >> 2) & 0x3) != 0x3);
 }
