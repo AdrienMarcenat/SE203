092ca689f53919af5d83fbeb511be89a17cbe126
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-30 10:11:33 +0100

    change init_bss to init_memory, which copy data, text and vtable from flash to ram, and zero out bss

--- TD/src/init.c
+++ TD/src/init.c
@@ -1,10 +1,25 @@
-extern char _bss_start, _bss_end;
+extern char _bss_start, _bss_end, _text_start, _text_end, _data_start, _data_end, _vtable_start, _vtable_end, _dataload, _textload, _vtableload;
 
-void init_bss()
+void init_memory()
 {
     char* i;
     for(i = &_bss_start; i < &_bss_end; i++)
     {
         *i = 0;   
+    }
+
+    for(i = &_data_start; i < &_data_end; i++)
+    {
+        *i = _dataload;   
+    }
+    
+    for(i = &_text_start; i < &_text_end; i++)
+    {
+        *i = _textload;   
+    }
+    
+    for(i = &_vtable_start; i < &_vtable_end; i++)
+    {
+        *i = _vtableload;   
     }
 }
